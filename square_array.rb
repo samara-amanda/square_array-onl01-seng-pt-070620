61954a0fc49f48257f713291b35b48b9aa641fbd
Samara Grant <samara.amanda01@gmail.com>
2020-07-13 02:45:15 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  new_numbers = []
+  array.each do |numbers|
+    final_numbers = numbers ** 2
+    new_numbers.push(final_numbers)
+  end
+  return new_numbers
 end
