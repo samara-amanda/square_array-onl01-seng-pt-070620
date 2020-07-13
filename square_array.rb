dd8ccd360e4d24137f664f17cd73f389d435e528
Samara Grant <samara.amanda01@gmail.com>
2020-07-13 02:34:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_numbers = []
   array.each do |numbers|
-    numbers ** 2
-    new_numbers.push(numbers)
+    final_numbers = numbers ** 2
+    new_numbers.push(final_numbers)
   end
   return new_numbers
 end
